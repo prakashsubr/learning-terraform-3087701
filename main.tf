a72cd64341f34e3078e3c765954d7d0001322e45
prakashsubr <47365322+prakashsubr@users.noreply.github.com>
2024-08-20 14:27:30 -0400

    autoscaling_commit

--- main.tf
+++ main.tf
@@ -33,19 +33,29 @@ module "blog_vpc" {
     Environment = "dev"
   }
 }
-resource "aws_instance" "blog" {
+
+
+module "autoscaling" {
+  source  = "devops-workflow/autoscaling/aws"
+  version = "3.2.1"
+
+
+  # insert the 10 required variables here
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog.vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id] 
+
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
 
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
