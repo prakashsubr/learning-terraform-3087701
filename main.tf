ae0a2e0f929037526b67deecec1bac0c04739f3c
prakashsubr <47365322+prakashsubr@users.noreply.github.com>
2024-08-21 10:53:24 -0400

    autoscaling_1

--- main.tf
+++ main.tf
@@ -43,9 +43,9 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns[0]
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id] 
-
+  
   image_id               = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
 
@@ -104,6 +104,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
+      target_id        = aws_instance.blog.id
      
     
     }
